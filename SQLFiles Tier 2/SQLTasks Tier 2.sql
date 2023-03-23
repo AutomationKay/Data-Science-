46672ac70cd783d2a62622875f2e0683dd9042ca
AutomationKay <112325655+AutomationKay@users.noreply.github.com>
2023-03-23 19:10:02 -0400

    SQL Updates

--- SQLFiles Tier 2/SQLTasks Tier 2.sql
+++ SQLFiles Tier 2/SQLTasks Tier 2.sql
@@ -34,27 +34,27 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* QUESTIONS 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
-SELECT * 
-FROM `Facilities` 
+SELECT name
+FROM Facilities
 WHERE membercost > 0;
 
 /* Q2: How many facilities do not charge a fee to members? */
 SELECT COUNT(*) 
-FROM `Facilities` 
+FROM Facilities
 WHERE membercost > 0;
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-SELECT facid, name, membercost, monthlymaintenance 
-FROM `Facilities` 
-WHERE membercost <= 20%monthlymaintenance AND membercost >0;
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+WHERE membercost > 0 AND membercost < 0.2 * monthlymaintenance;
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
 SELECT * 
-FROM `Facilities` 
+FROM Facilities 
 WHERE facid = 1 
 OR facid = 5;
 
@@ -71,19 +71,23 @@ FROM Facilities;
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 SELECT firstname, surname
-FROM `Members` 
+FROM Members
+WHERE joindate = (
+    SELECT MAX(joindate)
+    FROM Members
+);
+ 
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
-SELECT DISTINCT Members.surname, Facilities.name, Bookings.bookid, Members.memid, Facilities.facid
+SELECT DISTINCT Members.firstname || ' ' || Members.surname AS member_name, Facilities.name AS court_name
 FROM Members
-JOIN Bookings
-ON Members.memid = Bookings.memid
-JOIN Facilities
-ON Facilities.facid = Bookings.facid
-ORDER BY Members.surname;
+JOIN Bookings ON Members.memid = Bookings.memid
+JOIN Facilities ON Facilities.facid = Bookings.facid
+WHERE Facilities.name LIKE '%Tennis Court%'
+ORDER BY member_name;
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
